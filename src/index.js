e84eec2da70521adc294b63cda5367639b36019d
Cristian Pallarés <skyrpex@gmail.com>
2015-10-01 16:13:07 +0200

    Handles errors and uses sourcemaps

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import through2        from 'through2';
 import deepmerge       from 'deepmerge';
 import template        from 'lodash.template';
 import url             from 'rework-plugin-url';
+import convertSourceMap  from 'convert-source-map';
 import applySourceMap  from 'vinyl-sourcemaps-apply';
 
 const md5 = buffer => createHash('md5').update(buffer.toString()).digest('hex');
@@ -17,46 +18,55 @@ const md5 = buffer => createHash('md5').update(buffer.toString()).digest('hex');
 const compile = (name = '[hash][ext]') => {
 
   return through2.obj(function(file, enc, callback) {
+    try {
+      const handle = url => {
+        // Ignore data URLs.
+        if (url.match(/^data:/)) {
+          return url;
+        }
 
-    const handle = url => {
-      // Ignore data URLs.
-      if (url.match(/^data:/)) {
-        return url;
+        const obj = parse(url);
+        const filename = obj.pathname;
+        const contents = fs.readFileSync(filename);
+
+        const ext = path.extname(filename);
+        obj.pathname = template(name, { interpolate: /\[(.+?)\]/g })({
+          ext,
+          hash: md5(contents),
+          name: path.basename(filename, ext),
+        });
+
+        this.push(new File({
+          path: obj.pathname,
+          contents,
+        }));
+
+        return format(obj);
+      };
+
+      const sourceMapComment = convertSourceMap
+        .fromObject(file.sourceMap)
+        .toComment({ multiline: true });
+      const contents = file.contents.toString() + sourceMapComment;
+      const result = rework(contents, { source: file.relative })
+        .use(url(handle))
+        .toString({ sourcemap: true, sourcemapAsObject: true });
+
+      file.contents = new Buffer(result.code);
+
+      // Apply sourcemaps...
+      if (file.sourceMap) {
+        result.map.file = file.relative;
+        const map = convertSourceMap.fromObject(result.map).toJSON();
+        applySourceMap(file, map);
       }
 
-      const obj = parse(url);
-      const filename = obj.pathname;
-      const contents = fs.readFileSync(filename);
+      this.push(file);
+    } catch (error) {
+      this.emit('error', error);
+    }
 
-      const ext = path.extname(filename);
-      obj.pathname = template(name, { interpolate: /\[(.+?)\]/g })({
-        ext,
-        hash: md5(contents),
-        name: path.basename(filename, ext),
-      });
-
-      this.push(new File({
-        path: obj.pathname,
-        contents,
-      }));
-
-      return format(obj);
-    };
-
-    const result = rework(file.contents.toString())
-      .use(url(handle))
-      .toString({ sourcemap: true, sourcemapAsObject: true });
-
-    file.contents = new Buffer(result.code);
-
-    // TODO Apply sourcemaps... It doesn't work for now.
-    // if (file.sourceMap) {
-    //   applySourceMap(file, result.map.toString());
-    // }
-
-    this.push(file);
     callback();
-
   });
 
 };
